61b049ed5ebd51f87123f6f84abab57f0afcf4f4
rodrigodesousa <rodrigudesousa@gmail.com>
2021-07-21 10:47:41 -0400

    projeto inicial CRUD Despesa

---
+++ Mvc_FluxoCaixa/Controllers/despesasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_FluxoCaixa.Models;
+
+namespace Mvc_FluxoCaixa.Controllers
+{
+    public class despesasController : Controller
+    {
+        private FluxoCaixaEntities db = new FluxoCaixaEntities();
+
+        // GET: despesas
+        public ActionResult Index()
+        {
+            return View(db.despesa.ToList());
+        }
+
+        // GET: despesas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            despesa despesa = db.despesa.Find(id);
+            if (despesa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(despesa);
+        }
+
+        // GET: despesas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: despesas/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "despesaId,descricao")] despesa despesa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.despesa.Add(despesa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(despesa);
+        }
+
+        // GET: despesas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            despesa despesa = db.despesa.Find(id);
+            if (despesa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(despesa);
+        }
+
+        // POST: despesas/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "despesaId,descricao")] despesa despesa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(despesa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(despesa);
+        }
+
+        // GET: despesas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            despesa despesa = db.despesa.Find(id);
+            if (despesa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(despesa);
+        }
+
+        // POST: despesas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            despesa despesa = db.despesa.Find(id);
+            db.despesa.Remove(despesa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
